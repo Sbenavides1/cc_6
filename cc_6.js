76d906a863618a7cc2b2981c881f199dfccc1cad
Sofia Benavides <Sbenavidesarias@usf.edu>
2025-10-05 22:48:30 -0400

    Base Class

--- cc_6.js
+++ cc_6.js
@@ -1,2 +1,12 @@
 // Coding Challenge 6
 
+// Base Class
+class Employee {
+    constructor(name, department) {
+    this.name = name;
+    this.department = department;
+}
+
+describe () {
+    return `${this.name} works in the ${this.department} department.`;
+}
