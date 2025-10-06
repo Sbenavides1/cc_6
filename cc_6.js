ea47801c108d53084bed6654bb097e7c229f46fa
Sofia Benavides <Sbenavidesarias@usf.edu>
2025-10-05 23:19:23 -0400

    Adding databases

--- cc_6.js
+++ cc_6.js
@@ -26,4 +26,21 @@ describe() {
 }
 }
 
-//
+// Employees Database
+const e1 = new Employee("Munisa", "Finance");
+console.log(e1.describe());
+const e2 = new Employee("Daniel", "Marketing");
+console.log(e2.describe());
+const e3 = new Employee("Sarah", "IT");
+console.log(e3.describe());
+const e4 = new Employee("John", "Operations");
+console.log(e4.describe());
+const e5 = new Employee("Linda", "Customer Service");
+console.log(e5.describe());
+
+// Managers Database
+const m1 = new Manager("Carla", "HR", 5);
+console.log(m1.describe());
+const m2 = new Manager("Maria", "Sales", 10);
+console.log(m2.describe());
+const m3 = new Manager("Carlos", "Development", 8);
